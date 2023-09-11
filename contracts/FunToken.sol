18befe32bf0b270c279fa49064d044a415da60ee
Ramilbtc <ramadjamal@yandex.ru>
2023-09-11 08:54:19 +0300

    2

--- contracts/FunToken.sol
+++ contracts/FunToken.sol
@@ -1,2 +1,68 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
+
+contract FunToken {
+    string public name = "FunToken";
+    string public symbol = "FUN";
+    string public standart = "FunToken v.0.1";
+    uint256 public totalSupply;
+    uint256 public _userId;
+
+    address public ownerOfContract;
+    address[] public holderToken;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
+    mapping(address => TokenHolderInfo) public tokenHolderInfos;
+
+    struct TokenHolderInfo {
+        uint256 _tokenId;
+        address _from;
+        address _to;
+        uint256 _totalToken;
+        bool _tokenHolder;
+    }
+
+    mapping(address => uint256) public balanceOf;
+
+    constructor(uint256 _initialSupply) {
+        ownerOfContract = msg.sender;
+        balanceOf[ownerOfContract] = _initialSupply;
+        totalSupply = _initialSupply;
+    }
+
+    function inc() internal {
+        _userId++;
+    }
+
+    function transfer(
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value, "not money");
+        inc();
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        TokenHolderInfo storage tokenHolderInfo = tokenHolderInfos[_to];
+
+        tokenHolderInfo._to = _to;
+        tokenHolderInfo._from = msg.sender;
+        tokenHolderInfo._totalToken = _value;
+        tokenHolderInfo._tokenHolder = true;
+        tokenHolderInfo._tokenId = _userId;
+
+        holderToken.push(_to);
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+}
