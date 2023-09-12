80062e0e0127247ec7d47e855a3127553b8e6dae
Ramilbtc <ramadjamal@yandex.ru>
2023-09-12 07:08:37 +0300

    smart contract

--- contracts/FunToken.sol
+++ contracts/FunToken.sol
@@ -21,6 +21,8 @@ contract FunToken {
 
     mapping(address => TokenHolderInfo) public tokenHolderInfos;
 
+    mapping(address => mapping(address => uint256)) public allowance;
+
     struct TokenHolderInfo {
         uint256 _tokenId;
         address _from;
@@ -64,5 +66,47 @@ contract FunToken {
         emit Transfer(msg.sender, _to, _value);
 
         return true;
+    }
+
+    function approve(
+        address _spender,
+        uint256 _value
+    ) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint _value
+    ) public returns (bool success) {
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
+
+        return true;
+    }
+
+    function getTokenHolderData(
+        address _address
+    ) public view returns (uint256, address, address, uint256, bool) {
+        return (
+            tokenHolderInfos[_address]._tokenId,
+            tokenHolderInfos[_address]._from,
+            tokenHolderInfos[_address]._to,
+            tokenHolderInfos[_address]._totalToken,
+            tokenHolderInfos[_address]._tokenHolder
+        );
+    }
+
+    function getTokenHolder() public view returns (address[] memory) {
+        return holderToken;
     }
 }
